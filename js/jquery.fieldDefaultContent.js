35a8ea6a779c92f41ff601c319243b962037aa49
nico <nico@b0634e64-c1aa-11df-91ca-d707db8a7594>
2010-09-21 18:32:59 +0000

    Nettoyage de code
    git-svn-id: svn://10.177.10.5/projets/jquery_fielddefaultcontent/trunk@5 b0634e64-c1aa-11df-91ca-d707db8a7594

--- js/jquery.fieldDefaultContent.js
+++ js/jquery.fieldDefaultContent.js
@@ -1,95 +1,95 @@
-(function(window, $, undef) {
-	if($ === undef) {
-		throw new Error('Dépendence non satisfaite : jQuery');
-	}
+(function (window, Error, RegExp, $, undef) {
+    if ($ === undef) {
+        throw new Error('DÃ©pendence non satisfaite : jQuery');
+    }
 
-	var fieldDefaultContent = function(elem, sContent, sColor, bBold) {
-		var $elem = $(elem),
-		sOriginalColor, bOriginalBold, rPattern;
+    var fieldDefaultContent = function (elem, sContent, sColor, bBold) {
+        var $elem = $(elem),
+        sOriginalColor, sOriginalBackground, bOriginalBold, rPattern;
 
-		function focus() {
-			$elem
-				.stop()
-				.css({
-					color			: sOriginalColor,
-					'font-weight'	: bOriginalBold ? 'bold' : 'normal'
-				});
+        function focus() {
+            $elem
+                .stop()
+                .css({
+                    color            : sOriginalColor,
+                    'font-weight'    : bOriginalBold ? 'bold' : 'normal'
+                });
 
-			if($elem.val() === sContent) {
-				$elem.val('');
-			}
-		}
+            if ($elem.val() === sContent) {
+                $elem.val('');
+            }
+        }
 
-		function blur() {
-			if(rPattern.test($elem.val())) {
-				if($.fx.step.color) {
-					$elem
-						.css({
-							color			: sOriginalBackground,
-							'font-weight'	: (bBold || bOriginalBold) ? 'bold' : 'normal'
-						})
-						.val(sContent)
-						.animate(
-							{color		: sColor},
-							{
-								duration	: 'slow',
-								queue		: false
-							}
-						);
-				}
-				else {
-					$elem
-						.css({
-							color			: sColor,
-							'font-weight'	: bBold ? 'bold' : 'normal'
-						})
-						.val(sContent);
-				}
-			}
-		}
+        function blur() {
+            if (rPattern.test($elem.val())) {
+                if ($.fx.step.color) {
+                    $elem
+                        .css({
+                            color           : sOriginalBackground,
+                            'font-weight'   : (bBold || bOriginalBold) ? 'bold' : 'normal'
+                        })
+                        .val(sContent)
+                        .animate(
+                            {color          : sColor},
+                            {
+                                duration    : 'slow',
+                                queue       : false
+                            }
+                        );
+                }
+                else {
+                    $elem
+                        .css({
+                            color           : sColor,
+                            'font-weight'   : bBold ? 'bold' : 'normal'
+                        })
+                        .val(sContent);
+                }
+            }
+        }
 
-		function submit() {
-			if(rPattern.test($elem.val())) {
-				$elem
-					.stop()
-					.each(focus);
-			}
-		}
+        function submit() {
+            if (rPattern.test($elem.val())) {
+                $elem
+                    .stop()
+                    .each(focus);
+            }
+        }
 
-		function init() {
-			if(typeof sColor === 'boolean') {
-				bBold = sColor;
-				sColor = undef;
-			}
-			sColor = sColor || '#999';
-			sContent = sContent || $('label[for='+$elem.attr('id')+']:not(visible)').text();
-			rPattern = new RegExp('^(\\s+|'+sContent+')?$');
-			sOriginalColor = $elem.css('color');
-			bOriginalBold = $elem.css('font-weight') === 'bold';
-			sOriginalBackground = $elem.css('background-color') || 'white';
+        function init() {
+            if (typeof sColor === 'boolean') {
+                bBold = sColor;
+                sColor = undef;
+            }
+            sColor = sColor || '#999';
+            sContent = sContent || $('label[for=' + $elem.attr('id') + ']:not(visible)').text();
+            rPattern = new RegExp('^(\\s+|' + sContent + ')?$');
+            sOriginalColor = $elem.css('color');
+            bOriginalBold = $elem.css('font-weight') === 'bold';
+            sOriginalBackground = $elem.css('background-color') || 'white';
 
-			bBold = bBold === undef ? bOriginalBold : bBold;
+            bBold = bBold === undef ? bOriginalBold : bBold;
 
-			$elem
-				.focus(focus)
-				.blur(blur)
-				.each(function() {
-					$(this.form)
-						.submit(submit);
-				})
-				.blur();
-		}
+            $elem
+                .focus(focus)
+                .blur(blur)
+                .each(function () {
+                    $(this.form)
+                        .submit(submit);
+                })
+                .blur();
+        }
 
-		init();
-	};
+        init();
+    };
 
-	$.fn.fieldDefaultContent = function(sContent, sColor, bBold) {
-		return this
-			.filter('input, textarea')
-				.each(function() {
-					fieldDefaultContent(this, sContent, sColor, bBold);
-				})
-			.end();
-	};
+    $.fn.fieldDefaultContent = function (sContent, sColor, bBold) {
+        return this
+            .filter('input, textarea')
+                .each(function () {
+                    fieldDefaultContent(this, sContent, sColor, bBold);
+                })
+            .end();
+    };
 
-})(this, this.jQuery);
+}(this, this.Error, this.RegExp, this.jQuery));
